ff8293469f138bb757884ed426f5dbadfd77c3dc
data2 <muskteerathos@gmail.com>
2021-08-30 20:42:39 +0800

    测试sync.Map

---
+++ test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Product struct {
+	Name string
+}
+type Price struct {
+	Money int
+}
+func main()  {
+	var m sync.Map
+	var p = Product{
+		Name: "111",
+	}
+	var price = Price{
+		Money: 200,
+	}
+	m.Store(p, price)
+
+	val ,_ := m.Load(p)
+
+	fmt.Println(val.(Price).Money)
+
+}
