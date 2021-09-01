bb9b2d3b1dae020c0182674c7a5a6ffc80bcdc04
data2 <muskteerathos@gmail.com>
2021-09-01 19:52:30 +0800

    提成单独db模块

--- db.go
+++ db.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
+	"net"
 	"sync"
+)
+
+const (
+	GET    = "get"
+	GETALL = "getall"
+	SET    = "set"
+	DEL    = "del"
+	DELALL = "delall"
+	NOTIFY = "notify"
 )
 
 type Response struct {
@@ -26,6 +37,16 @@ const (
 	UNAUTH  = 2
 )
 
+func (r Response) Return(conn *net.TCPConn, action string) {
+	r.Action = action
+	message := ToJsonString(r)
+	fmt.Println(message)
+	_, err := conn.Write([]byte(message + "\n"))
+	if err != nil {
+		fmt.Println("send message fail ", err)
+	}
+}
+
 func DelData(db *gorm.DB, namespace string, path string) Response {
 	err := db.Delete(&Record{}, "namespace =? and path = ?", namespace, path).Error
 	if err != nil {
@@ -37,20 +58,28 @@ func DelData(db *gorm.DB, namespace string, path string) Response {
 var lock = sync.Mutex{}
 
 func SetData(db *gorm.DB, namespace string, path string, value string) Response {
+	defer lock.Unlock()
 	lock.Lock()
-	record := LoadData(db, namespace, path)
+	response := LoadData(db, namespace, path)
 	var err error
-	if record.Code == SUCCESS && record.Data != (Record{}) {
-		err = db.Model(&(record.Data)).Update(path, value).Error
+	var record Record
+	if response.Code == SUCCESS && response.Data != (Record{}) {
+		record = response.Data.(Record)
+		err = db.Model(&(record)).Where("path", path).Update("value", value).Error
+		if err != nil {
+			fmt.Println("update data fail > " + err.Error())
+			return Response{Code: FAIL, Message: "update data fail. "}
+		}
 	} else {
 		uid, _ := uuid.NewUUID()
-		err = db.Create(&Record{Namespace: namespace, ID: uid.String(), Path: path, Value: value}).Error
+		record = Record{Namespace: namespace, ID: uid.String(), Path: path, Value: value}
+		err = db.Create(&record).Error
+		if err != nil {
+			fmt.Println("set data fail > " + err.Error())
+			return Response{Code: FAIL, Message: "set data fail. "}
+		}
 	}
-	lock.Unlock()
-	if err != nil {
-		return Response{Code: FAIL, Message: "set data fail."}
-	}
-	return Response{Code: SUCCESS}
+	return Response{Code: SUCCESS, Data: record}
 }
 
 func LoadData(db *gorm.DB, namespace string, path string) Response {
