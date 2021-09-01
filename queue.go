7b4998a2cc92d9649f2e9cd80a153112548cebd2
data2 <muskteerathos@gmail.com>
2021-09-01 19:53:40 +0800

    基于redis的发布订阅实现的跨服>通信

--- queue.go
+++ queue.go
@@ -1,28 +1,41 @@
 package main
 
 import (
+	"container/list"
 	"github.com/go-redis/redis"
 	"log"
+	"net"
 	"sync"
 	"time"
 )
 
-func PublishMessage(rdb *redis.Client , record Record)  {
-	res, err := rdb.Publish("queue:confcarrier", ToJsonString(record)).Result()
+func PublishMessage(rdb *redis.Client, record Record) {
+	res, err := rdb.Publish("queueconfcarrier", ToJsonString(record)).Result()
 	if err != nil {
-		log.Println(err,res)
-		time.Sleep(time.Duration(2)*time.Second)
+		log.Println(err, res)
+		time.Sleep(time.Duration(2) * time.Second)
 		PublishMessage(rdb, record)
 	}
 }
 
-
-func SubscribeMessage(rdb *redis.Client, queue sync.Map) {
-	pubSub := rdb.Subscribe("queue:confcarrier")
+func SubscribeMessage(rdb *redis.Client, m *sync.Map) {
+	pubSub := rdb.Subscribe("queueconfcarrier")
 	ch := pubSub.Channel()
-	for msg := range ch{
-		recordJsonstr := msg.Payload
-		BroadcastByNamespace(ToInterface(recordJsonstr).Namespace, recordJsonstr)
+	for msg := range ch {
+		record := ToInterface(msg.Payload)
+		val, _ := m.Load(record.Namespace)
+		if val == nil {
+		} else {
+			queue := val.(*list.List)
+			for i := queue.Front(); i != nil; i = i.Next() {
+				c := i.Value.(*net.TCPConn)
+				if c != nil {
+					Response{
+						Code: SUCCESS,
+						Data: record,
+					}.Return(c, NOTIFY)
+				}
+			}
+		}
 	}
 }
-
