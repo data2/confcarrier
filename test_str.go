a40d809519e23f06ba774d70a64322728308c565
data2 <muskteerathos@gmail.com>
2021-09-01 19:54:19 +0800

    str test

---
+++ test_str.go
@@ -0,0 +1,12 @@
+package main
+
+//func main()  {
+//	//strs := strings.Split("test|1534c9b83ac6ede0a791503ade12d72a|getall", "|")
+//	//fmt.Println(len(strs))
+//	//fmt.Println(strs[2])
+//
+//	var r Record
+//	s := `{"ID":"101a7cb6-0aff-11ec-b26b-88e9fe840b9a","Namespace":"test","Path":"kkk","Value":"uuuu1"}`
+//	json.Unmarshal([]byte(s), &r)
+//	fmt.Println(r)
+//}
