f88e882ef2b0a58d3736687d475dbab00374b891
data2 <muskteerathos@gmail.com>
2023-07-25 17:03:53 +0800

    Update priority_queue.go

--- priority_queue.go
+++ priority_queue.go
@@ -5,47 +5,47 @@ import (
 	"fmt"
 )
 
-type ListNode struct {
+type ConfServerNode struct {
 	Val  int
-	Next *ListNode
+	Next *ConfServerNode
 }
 
 // 定义一个优先级队列
-type PriorityQueue []*ListNode
+type PriorityConfServerQueue []*ConfServerNode
 
-func (p PriorityQueue) Len() int {
+func (priorityConfServerQueue PriorityConfServerQueue) Len() int {
 	return len(p)
 }
 
 // 小于(<)就是小顶堆, 大于(>)就是大顶堆
-func (p PriorityQueue) Less(i, j int) bool {
+func (priorityConfServerQueue PriorityConfServerQueue) Less(i, j int) bool {
 	return p[i].Val < p[j].Val
 }
-func (p PriorityQueue) Swap(i, j int) {
+func (priorityConfServerQueue PriorityConfServerQueue) Swap(i, j int) {
 	p[i], p[j] = p[j], p[i]
 	return
 }
 
-func (p *PriorityQueue) Push(x interface{}) {
-	*p = append(*p, x.(*ListNode))
+func (priorityConfServerQueue *PriorityConfServerQueue) Push(x interface{}) {
+	*priorityConfServerQueue = append(*p, x.(*ConfServerNode))
 }
 
-func (p *PriorityQueue) Pop() interface{} {
+func (priorityConfServerQueue *PriorityConfServerQueue) Pop() interface{} {
 	old := *p
 	n := len(old)
 	x := old[n-1]
-	*p = old[0 : n-1]
+	*priorityConfServerQueue = old[0 : n-1]
 	return x
 }
 
 func main() {
-	p := &PriorityQueue{}
+	priorityConfServerQueue := &PriorityConfServerQueue{}
 	heap.Init(p)
-	node1 := &ListNode{Val: 4}
-	node2 := &ListNode{Val: 3}
-	node3 := &ListNode{Val: 2}
-	node4 := &ListNode{Val: 6}
-	node5 := &ListNode{Val: 1}
+	node1 := &ConfServerNode{Val: 4}
+	node2 := &ConfServerNode{Val: 3}
+	node3 := &ConfServerNode{Val: 2}
+	node4 := &ConfServerNode{Val: 6}
+	node5 := &ConfServerNode{Val: 1}
 	heap.Push(p, node1)
 	heap.Push(p, node2)
 	heap.Push(p, node3)
@@ -54,6 +54,6 @@ func main() {
 
 	for p.Len() > 0 {
 		node := heap.Pop(p)
-		fmt.Print(node.(*ListNode).Val, " ")
+		fmt.Print(node.(*ConfServerNode).Val, " ")
 	}
 }
